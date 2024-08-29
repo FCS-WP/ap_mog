ff42f1dc4ab580154f0dea0e9a3e4496dc65af43
shin <nhathanh024@gmail.com>
2024-08-29 14:01:21 +0700

    init code

---
+++ src/wp-content/themes/weiboo-child/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+/*** Child Theme Function  ***/
+function weiboo_theme_enqueue_scripts() {
+	wp_register_style( 'childstyle', get_template_directory_uri() . '/style.css'  );
+	wp_enqueue_style( 'childstyle' );
+}
+add_action('wp_enqueue_scripts', 'weiboo_theme_enqueue_scripts', 11);
+/*
+ * Define Variables
+ */
+if (!defined('THEME_DIR'))
+    define('THEME_DIR', get_template_directory());
+if (!defined('THEME_URL'))
+    define('THEME_URL', get_template_directory_uri());
+
+
+/*
+ * Include framework files
+ */
+foreach (glob(THEME_DIR.'-child' . "/includes/*.php") as $file_name) {
+    require_once ( $file_name );
+}
+
+
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
